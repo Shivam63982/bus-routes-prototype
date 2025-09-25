93ef43261f25797bcbe811be68253c8fb0be693d
Shivam63982 <memaram9090@gmail.com>
2025-09-25 14:44:44 +0530

    Update routes.js

--- pages/api/routes.js
+++ pages/api/routes.js
@@ -1,5 +1,18 @@
-import routes from "../../routes.json";
-
 export default function handler(req, res) {
-  res.status(200).json(routes);
+  res.status(200).json({
+    "routes": [
+      {
+        "route_name": "isbt-ronkta",
+        "bus_id": "bus_id43",
+        "initial_stop": { "name": "ISBT", "lat": 27.2000, "lon": 78.0100 },
+        "final_stop": { "name": "Ronkta", "lat": 27.2500, "lon": 78.0500 }
+      },
+      {
+        "route_name": "khandari-bhagwan",
+        "bus_id": "bus_id87",
+        "initial_stop": { "name": "Khandari", "lat": 27.1900, "lon": 78.0200 },
+        "final_stop": { "name": "Bhagwan Talkies", "lat": 27.2200, "lon": 78.0400 }
+      }
+    ]
+  });
 }
